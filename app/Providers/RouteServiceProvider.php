c1ba15abf6117aba15b1e92d077bdaed40157977
Fujita Namiko <fujita-namiko@elecs-web.co.jp>
2022-05-26 16:52:22 +0900

    contactフォーム2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,6 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
+    protected $namespace = 'App\\Http\\Controllers';
     public const HOME = '/home';
 
     /**
@@ -34,6 +35,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+             // 追加箇所
+             ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactsController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,21 +17,30 @@ use App\Http\Controllers\ContactsController;
 
 // お問い合わせ入力ページ
 // Route::get('/', 'ContactsController@index')->name('contact');
-Route::get('/contact', [ContactsController::class, 'contact']);
-// Route::get('/', 'App\Http\Controllers\ContactsController@contact' );
+// Route::get('/contact', [ContactController::class, 'index']);
+// Route::get('/', 'App\Http\Controllers\ContactsController@index')->name(‘contact’);
+
+Route::get('/', [ContactsController::class, 'index'])->name('contact.index');
+
 
 // 確認ページ
 // Route::post('/confirm', 'ContactsController@confirm')->name('confirm');
-Route::post('/confirm', [ContactsController::class, 'confirm']);
-// Route::get('/confirm', 'App\Http\Controllers\ContactsController@confirm' );
+// Route::post('/contact/confirm', [ContactsController::class, 'confirm']);
+// Route::get('/confirm', 'App\Http\Controllers\ContactsController@confirm')->name(‘confirm’);
+
+Route::get('/confirm', [ContactsController::class, 'confirm'])->name('confirm');
 
 // DB挿入、メール送信
 // Route::post('/process', 'ContactsController@process')->name('process');
-Route::post('/process', [ContactsController::class, 'process']);
-// Route::get('/process', 'App\Http\Controllers\ContactsController@process' );
+// Route::post('contact/process', [ContactsController::class, 'process']);
+// Route::get('/process', 'App\Http\Controllers\ContactsController@process')->name(‘process’);
 
-// 完了ページs
+Route::get('/process', [ContactsController::class, 'process'])->name('process');
+
+// 完了ページ
 
 // Route::get('/complete', 'ContactsController@complete')->name('complete');
- Route::get('/complete', [ContactsController::class, 'complete']);
-// Route::get('/complete', 'App\Http\Controllers\ContactsController@complete' );
+//  Route::get('contact/complete', [ContactsController::class, 'complete']);
+// Route::get('/complete', 'App\Http\Controllers\ContactsController@complete')->name(‘complete’);
+
+Route::get('/complete', [ContactsController::class, 'complete'])->name('complete');
